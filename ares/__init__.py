44a3822936a39ec614076196d357063dabcef20d
Fionn Fitzmaurice <iamfionn@github.com>
2018-10-30 00:12:40 +0800

    Bump minor version to 0.6

--- ares/__init__.py
+++ ares/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'ares'
-__version__ = '0.5'
+__version__ = '0.6'
 __author__ = 'Martin Simon <me@martinsimon.me>'
 __repo__ = 'https://github.com/barnumbirr/ares'
 __license__ = 'Apache v2.0 License'
